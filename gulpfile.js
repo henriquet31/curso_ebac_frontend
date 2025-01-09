dcad8566ac51111c52e598e3aae0f0cb1a5c2c99
henriquet31 <henriquet023@outlook.com>
2025-01-09 19:50:14 -0300

    refactor: improve Gulp tasks structure and add watch functionality for images, scripts, and styles

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,18 @@ const uglify = require('gulp-uglify');
 const obfuscate = require('gulp-obfuscate');
 const imagemin = require('gulp-imagemin');
 
-function comprimeImagens(){
-    return gulp.src('./source/images/*', {encoding: false,})
-    .pipe(imagemin())
-    .pipe(gulp.dest('./build/images'));
+function comprimeImagens() {
+    return gulp.src('./source/images/*', { encoding: false })
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'));
 }
 
 function comprimeJavaScript() {
     return gulp.src('./source/scripts/*.js')
         .pipe(uglify())
         .pipe(obfuscate())
-        .pipe(gulp.dest('./build/scripts'));}
+        .pipe(gulp.dest('./build/scripts'));
+}
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
@@ -27,8 +28,13 @@ function compilaSass() {
         .pipe(gulp.dest('./build/styles'));
 }
 
-exports.default = function() {
+function watchFiles() {
     gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
     gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
     gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
 }
+
+exports.default = gulp.series(
+    gulp.parallel(comprimeImagens, comprimeJavaScript, compilaSass),
+    watchFiles
+);
