b1b05b32a25bb16420bba431f3669631ce6550b6
henriquet31 <henriquet023@outlook.com>
2025-01-09 16:51:18 -0300

    add gulp-sourcemaps for improved debugging and update color variable to RGB format

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
         }))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./build/styles'));
 }
 
 exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+};
+
+gulp.task('default', gulp.series(compilaSass, exports.watch));
