4505904fae6c08750c5bc0ab18b0d84cdeb6dcf1
henriquet31 <henriquet023@outlook.com>
2025-04-17 18:37:23 -0300

    Refactor to use classes corrigindo código

--- main.js
+++ main.js
@@ -1,36 +1,44 @@
-function criarPessoa(nome, idade, cpf, email) {
-    return {
-        nome: nome,
-        idade: idade,
-        cpf: cpf,
-        email: email
-    };
+class Pessoa {
+    constructor(nome, idade, cpf, email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    exibirDados() {
+        return `Nome: ${this.nome}, Idade: ${this.idade}, CPF: ${this.cpf}, Email: ${this.email}`;
+    }
 }
 
-function criarEstudante(nome, idade, cpf, email, curso) {
-    let estudante = criarPessoa(nome, idade, cpf, email);
-    estudante.curso = curso;
-    estudante.exibirDados = function() {
-        return `Nome: ${this.nome}, Idade: ${this.idade}, CPF: ${this.cpf}, Email: ${this.email}, Curso: ${this.curso}`;
-    };
-    return estudante;
+class Estudante extends Pessoa {
+    constructor(nome, idade, cpf, email, curso) {
+        super(nome, idade, cpf, email);
+        this.curso = curso;
+    }
+
+    exibirDados() {
+        return `${super.exibirDados()}, Curso: ${this.curso}`;
+    }
 }
 
-function criarProfessor(nome, idade, cpf, email, disciplina) {
-    let professor = criarPessoa(nome, idade, cpf, email);
-    professor.disciplina = disciplina;
-    professor.exibirDados = function() {
-        return `Nome: ${this.nome}, Idade: ${this.idade}, CPF: ${this.cpf}, Email: ${this.email}, Disciplina: ${this.disciplina}`;
-    };
-    return professor;
+class Professor extends Pessoa {
+    constructor(nome, idade, cpf, email, disciplina) {
+        super(nome, idade, cpf, email);
+        this.disciplina = disciplina;
+    }
+
+    exibirDados() {
+        return `${super.exibirDados()}, Disciplina: ${this.disciplina}`;
+    }
 }
 
-const pessoa1 = criarPessoa("Henrique", 25, "123.456.789-00", "henrique@email.com");
-const estudante1 = criarEstudante("Ana", 20, "987.654.321-00", "ana@email.com", "Engenharia");
-const professor1 = criarProfessor("Carlos", 40, "456.789.123-00", "carlos@email.com", "Matemática");
+const pessoa1 = new Pessoa("Henrique", 25, "123.456.789-00", "henrique@email.com");
+const estudante1 = new Estudante("Ana", 20, "987.654.321-00", "ana@email.com", "Engenharia");
+const professor1 = new Professor("Carlos", 40, "456.789.123-00", "carlos@email.com", "Matemática");
 
 function mostrarDados(pessoas) {
-    pessoas.forEach(pessoa => console.log(pessoa.exibirDados ? pessoa.exibirDados() : pessoa));
+    pessoas.forEach(pessoa => console.log(pessoa.exibirDados()));
 }
 
 mostrarDados([pessoa1, estudante1, professor1]);
