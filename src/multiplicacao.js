bcc8f2bd4ae0314ed6df0289b3b5b39acc6c4b34
henriquet31 <henriquet023@outlook.com>
2025-04-30 20:46:09 -0300

    Primeiro commit

---
+++ src/multiplicacao.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá, ${nome}!`;
+}
+console.log(multiplicacao(8, 5));
+console.log(saudacao("Henrique"));
