00a5860434264f388089cf103c8b2e465fce6937
henriquet31 <henriquet023@outlook.com>
2025-04-30 20:59:37 -0300

    Removendo arquivos da branch main

--- src/multiplicacao.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function multiplicacao(a, b) {
-    return a * b;
-}
-function saudacao(nome) {
-    return `Olá, ${nome}!`;
-}
-console.log(multiplicacao(8, 5));
-console.log(saudacao("Henrique"));
